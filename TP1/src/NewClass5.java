0064846b8c03c91f1f8807e1586955792cfd5fc7
gabinete <gabinete@mail.com>
2023-09-05 16:52:45 -0300

    tp1

---
+++ TP1/src/NewClass5.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author estudiante
+ */
+public class NewClass5{
+    public static  void main(String[] args) {
+        int num1;
+        num1=1;
+        float num2= 1.2f;
+        boolean pepe;
+        long num3 = 12345;
+        System.out.println("El numero es " + num1);
+        ArrayList<Float> lista =new ArrayList <> ();
+        lista.add (1f);
+        lista.add (4f);
+        lista.add(10f);
+        lista.add(15f);
+       for(float i:lista){
+           
+           int contador = 0;
+           contador++;
+           
+       }
+       }
+ }
+    
+
+ 
+
+ 
+   
+
